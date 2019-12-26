a0e91418d6b9cd45edf80c74d284da6128674100
NoflexGit <untroman@mail.ru>
2019-12-27 00:01:49 +0300

    "Apple and Oranges" has been updated with shorter version

---
+++ Apple and Orange/appleAndOrange.js
@@ -0,0 +1,15 @@
+const countApplesAndOranges = (s, t, a, b, apples, oranges) => {
+  const countSumm = (fruits, treePosition) => {
+    let result = 0;
+    fruits.forEach(fruit => {
+      const fruitPosition = treePosition + fruit;
+      if (fruitPosition >= s && fruitPosition <= t) result++;
+    });
+    return result;
+  };
+
+  console.log(countSumm(apples, a));
+  console.log(countSumm(oranges, b));
+};
+
+countApplesAndOranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4]);
