1cc05cb7af7d494638313564680145d860e7c64b
NoflexGit <untroman@mail.ru>
2019-12-27 00:03:09 +0300

    Marked examples

--- Apple and Orange/appleAndOrange.js
+++ Apple and Orange/appleAndOrange.js
@@ -12,4 +12,5 @@ const countApplesAndOranges = (s, t, a, b, apples, oranges) => {
   console.log(countSumm(oranges, b));
 };
 
+// Example
 countApplesAndOranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4]);
