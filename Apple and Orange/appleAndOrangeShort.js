788e8e4fb4b9a793f7428d198822e6bc853e89dd
NoflexGit <untroman@mail.ru>
2019-12-27 00:00:51 +0300

    "Apple and Oranges" has been added

---
+++ Apple and Orange/appleAndOrangeShort.js
@@ -0,0 +1,11 @@
+const countApplesAndOranges = (s, t, a, b, apples, oranges) => {
+  const countSumm = (fruits, treePos) =>
+    fruits.filter(
+      fruitPos => fruitPos + treePos >= s && fruitPos + treePos <= t
+    ).length;
+
+  console.log(countSumm(apples, a));
+  console.log(countSumm(oranges, b));
+};
+
+countApplesAndOranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4]);
